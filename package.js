218fb01d9733acc2ca67d3b0547080f9015fdb8d
Lukasz Jagodzinski <luke.jagodzinski@gmail.com>
2015-05-03 23:30:12 +0200

    Add new helpers

--- package.js
+++ package.js
@@ -8,7 +8,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
 
-  api.use('jagi:astronomy@0.4.0');
+  api.use('jagi:astronomy@0.4.1');
   api.use('underscore');
   api.use('reactive-dict');
   api.use('reactive-var');
