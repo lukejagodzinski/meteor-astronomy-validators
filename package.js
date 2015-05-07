902e2a207abad4cb7eff386b10079d8d69460025
jagi <luke.jagodzinski@gmail.com>
2015-05-07 15:01:44 +0200

    New events system

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jagi:astronomy-validators',
-  version: '0.4.1',
+  version: '0.5.0',
   summary: 'Validators for Meteor Astronomy',
   git: 'https://github.com/jagi/meteor-astronomy-validators.git'
 });
@@ -8,11 +8,13 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
 
-  api.use('jagi:astronomy@0.4.1');
+  api.use('jagi:astronomy@0.5.0');
   api.use('underscore');
   api.use('reactive-dict');
   api.use('reactive-var');
   api.use('ui');
+
+  api.imply('jagi:astronomy');
 
   // Module.
   api.addFiles('lib/module/global.js', ['client', 'server']);
