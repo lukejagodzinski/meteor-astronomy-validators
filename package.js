9f2821a5f165c973139e2485b7cbe5d600e17263
Lukasz Jagodzinski <luke.jagodzinski@gmail.com>
2015-05-24 13:03:09 +0200

    Bump version to 0.8.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jagi:astronomy-validators',
-  version: '0.8.1',
+  version: '0.8.2',
   summary: 'Validators for Meteor Astronomy',
   git: 'https://github.com/jagi/meteor-astronomy-validators.git'
 });
