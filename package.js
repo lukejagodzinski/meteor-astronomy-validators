c8e095a9b980b695af7dda4686c8b7fc7d5f3287
Lukasz Jagodzinski <luke.jagodzinski@gmail.com>
2015-05-31 15:18:33 +0200

    Remove validators aliases

--- package.js
+++ package.js
@@ -26,30 +26,31 @@ Package.onUse(function(api) {
   api.addFiles('lib/module/module.js', ['client', 'server']);
 
   // Validators.
-  api.addFiles('lib/validators/is_string.js', ['client', 'server']);
-  api.addFiles('lib/validators/is_number.js', ['client', 'server']);
-  api.addFiles('lib/validators/is_array.js', ['client', 'server']);
-  api.addFiles('lib/validators/is_object.js', ['client', 'server']);
-  api.addFiles('lib/validators/is_date.js', ['client', 'server']);
-  api.addFiles('lib/validators/is_null.js', ['client', 'server']);
-  api.addFiles('lib/validators/is_not_null.js', ['client', 'server']);
-  api.addFiles('lib/validators/is_email.js', ['client', 'server']);
+  api.addFiles('lib/validators/string.js', ['client', 'server']);
+  api.addFiles('lib/validators/number.js', ['client', 'server']);
+  api.addFiles('lib/validators/array.js', ['client', 'server']);
+  api.addFiles('lib/validators/object.js', ['client', 'server']);
+  api.addFiles('lib/validators/date.js', ['client', 'server']);
+  api.addFiles('lib/validators/email.js', ['client', 'server']);
 
   api.addFiles('lib/validators/required.js', ['client', 'server']);
+  api.addFiles('lib/validators/null.js', ['client', 'server']);
+  api.addFiles('lib/validators/not_null.js', ['client', 'server']);
+
+  api.addFiles('lib/validators/length.js', ['client', 'server']);
   api.addFiles('lib/validators/min_length.js', ['client', 'server']);
   api.addFiles('lib/validators/max_length.js', ['client', 'server']);
-  api.addFiles('lib/validators/length.js', ['client', 'server']);
-  api.addFiles('lib/validators/greater_than.js', ['client', 'server']);
-  api.addFiles('lib/validators/greater_than_or_equal.js', ['client', 'server']);
-  api.addFiles('lib/validators/less_than.js', ['client', 'server']);
-  api.addFiles('lib/validators/less_than_or_equal.js', ['client', 'server']);
-  api.addFiles('lib/validators/has_property.js', ['client', 'server']);
+  api.addFiles('lib/validators/gt.js', ['client', 'server']);
+  api.addFiles('lib/validators/gte.js', ['client', 'server']);
+  api.addFiles('lib/validators/lt.js', ['client', 'server']);
+  api.addFiles('lib/validators/lte.js', ['client', 'server']);
 
+  api.addFiles('lib/validators/has.js', ['client', 'server']);
+  api.addFiles('lib/validators/choice.js', ['client', 'server']);
+  api.addFiles('lib/validators/unique.js', ['client', 'server']);
   api.addFiles('lib/validators/equal.js', ['client', 'server']);
   api.addFiles('lib/validators/equal_to.js', ['client', 'server']);
   api.addFiles('lib/validators/regexp.js', ['client', 'server']);
-  api.addFiles('lib/validators/choice.js', ['client', 'server']);
-  api.addFiles('lib/validators/unique.js', ['client', 'server']);
 
   api.export(['Validators'], ['client', 'server']);
 });
