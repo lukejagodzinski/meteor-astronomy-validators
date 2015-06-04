4424553cf4e6debb918549d128bbaa1fcbf3f42c
Lukasz Jagodzinski <luke.jagodzinski@gmail.com>
2015-06-04 22:05:34 +0200

    Ability to define validators in fields definition

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jagi:astronomy-validators',
-  version: '0.10.5',
+  version: '0.10.6',
   summary: 'Validators for Meteor Astronomy',
   git: 'https://github.com/jagi/meteor-astronomy-validators.git'
 });
