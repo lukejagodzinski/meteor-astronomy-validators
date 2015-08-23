0650e0c9792ea3204a7609f63a9505ef12ed6d8d
Lukasz Jagodzinski <luke.jagodzinski@gmail.com>
2015-08-23 17:58:07 +0200

    New before/afterInit events

--- package.js
+++ package.js
@@ -17,49 +17,60 @@ Package.onUse(function(api) {
   api.imply('jagi:astronomy');
 
   // Module.
-  api.addFiles('lib/module/global.js', ['client', 'server']);
-  api.addFiles('lib/module/error.js', ['client', 'server']);
-  api.addFiles('lib/module/utils.js', ['client', 'server']);
-  api.addFiles('lib/module/validator_definition.js', ['client', 'server']);
-  api.addFiles('lib/module/validator.js', ['client', 'server']);
-  api.addFiles('lib/module/init_module.js', ['client', 'server']);
-  api.addFiles('lib/module/init_class.js', ['client', 'server']);
-  api.addFiles('lib/module/init_instance.js', ['client', 'server']);
+  api.addFiles([
+    'lib/module/global.js',
+    'lib/module/error.js',
+    'lib/module/utils.js',
+    'lib/module/validator_definition.js',
+    'lib/module/validator.js',
+    'lib/module/init_module.js',
+    'lib/module/init_class.js'
+  ], ['client', 'server']);
 
   // Type validators.
-  api.addFiles('lib/validators/type/string.js', ['client', 'server']);
-  api.addFiles('lib/validators/type/number.js', ['client', 'server']);
-  api.addFiles('lib/validators/type/boolean.js', ['client', 'server']);
-  api.addFiles('lib/validators/type/array.js', ['client', 'server']);
-  api.addFiles('lib/validators/type/object.js', ['client', 'server']);
-  api.addFiles('lib/validators/type/date.js', ['client', 'server']);
-  api.addFiles('lib/validators/type/email.js', ['client', 'server']);
+  api.addFiles([
+    'lib/validators/type/string.js',
+    'lib/validators/type/number.js',
+    'lib/validators/type/boolean.js',
+    'lib/validators/type/array.js',
+    'lib/validators/type/object.js',
+    'lib/validators/type/date.js',
+    'lib/validators/type/email.js'
+  ], ['client', 'server']);
 
   // Existence validators.
-  api.addFiles('lib/validators/existence/required.js', ['client', 'server']);
-  api.addFiles('lib/validators/existence/null.js', ['client', 'server']);
-  api.addFiles('lib/validators/existence/not_null.js', ['client', 'server']);
-  api.addFiles('lib/validators/existence/has.js', ['client', 'server']);
+  api.addFiles([
+    'lib/validators/existence/required.js',
+    'lib/validators/existence/null.js',
+    'lib/validators/existence/not_null.js',
+    'lib/validators/existence/has.js'
+  ], ['client', 'server']);
 
   // Size validators.
-  api.addFiles('lib/validators/size/length.js', ['client', 'server']);
-  api.addFiles('lib/validators/size/min_length.js', ['client', 'server']);
-  api.addFiles('lib/validators/size/max_length.js', ['client', 'server']);
-  api.addFiles('lib/validators/size/gt.js', ['client', 'server']);
-  api.addFiles('lib/validators/size/gte.js', ['client', 'server']);
-  api.addFiles('lib/validators/size/lt.js', ['client', 'server']);
-  api.addFiles('lib/validators/size/lte.js', ['client', 'server']);
+  api.addFiles([
+    'lib/validators/size/length.js',
+    'lib/validators/size/min_length.js',
+    'lib/validators/size/max_length.js',
+    'lib/validators/size/gt.js',
+    'lib/validators/size/gte.js',
+    'lib/validators/size/lt.js',
+    'lib/validators/size/lte.js'
+  ], ['client', 'server']);
 
   // Comparison validators.
-  api.addFiles('lib/validators/comparison/choice.js', ['client', 'server']);
-  api.addFiles('lib/validators/comparison/unique.js', ['client', 'server']);
-  api.addFiles('lib/validators/comparison/equal.js', ['client', 'server']);
-  api.addFiles('lib/validators/comparison/equal_to.js', ['client', 'server']);
-  api.addFiles('lib/validators/comparison/regexp.js', ['client', 'server']);
+  api.addFiles([
+    'lib/validators/comparison/choice.js',
+    'lib/validators/comparison/unique.js',
+    'lib/validators/comparison/equal.js',
+    'lib/validators/comparison/equal_to.js',
+    'lib/validators/comparison/regexp.js'
+  ], ['client', 'server']);
 
   // Logical operators.
-  api.addFiles('lib/validators/logical/and.js', ['client', 'server']);
-  api.addFiles('lib/validators/logical/or.js', ['client', 'server']);
+  api.addFiles([
+    'lib/validators/logical/and.js',
+    'lib/validators/logical/or.js'
+  ], ['client', 'server']);
 
   api.export(['Validators'], ['client', 'server']);
 });
